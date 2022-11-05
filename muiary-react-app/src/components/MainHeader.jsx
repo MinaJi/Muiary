a32de0d50d73c416d7f205adbbf3c81302d2aa24
MinaJi <minaaa0820@gmail.com>
2022-11-05 23:55:11 +0900

    header

--- muiary-react-app/src/components/MainHeader.jsx
+++ muiary-react-app/src/components/MainHeader.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { Grid } from "@mui/material";
-import { Container } from "@mui/system";
+import { CiLight, CiDark } from "react-icons/ci";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 const Header = styled(Grid)`
   && {
@@ -17,38 +28,100 @@ const Header = styled(Grid)`
 
 const Btn = styled.button`
   background-color: inherit;
-  border: 1px solid black;
-  border-radius: 30px;
+  border: 1px solid #cccccc;
+  border-radius: 10px;
   width: 70px;
   height: 40px;
+  margin: 3px;
+  cursor: pointer;
 `;
+
+const TextBtn = styled.button`
+  cursor: pointer;
+  border: none;
+  background-color: inherit;
+  font-size: 15px;
+  font-weight: bold;
+`
 
 const Img = styled.img`
-  width: 115px;
+  height: 50px;
+  display: flex;
 `;
 
+const pages = ["Products", "Pricing", "Blog"];
+
 function MainHeader() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <>
-      <Header container alignItems="center">
-        <Container maxWidth="xs">
-          <Grid item xs={2} style={{backgroundColor:"red"}}>
-            <Img src="/static/logoonlytext.png" alt="logo" />
-          </Grid>
-          <Grid item xs={4}>
-            안녕
-          </Grid>
-          {/* <Grid item xs={1} style={{backgroundColor:"red"}}>
-            <div>
-              <Btn>Sign up</Btn>
-            </div>
-            <div>
-              <Btn>Sign in</Btn>
-            </div>
-          </Grid> */}
-        </Container>
-      </Header>
-    </>
+    <Header position="static" alignItems="ceter">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Img src="/static/logoonlytext.png" alt="logo" />
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+              style={{ color: "black" }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <TextBtn
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "black", display: "block" }}
+              >
+                {page}
+              </TextBtn>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Btn>Sign up</Btn>
+            <Btn>Sign in</Btn>
+            <CiDark style={{fontSize:"inherite"}}/>
+          </Box>
+        </Toolbar>
+      </Container>
+    </Header>
   );
 }
 
