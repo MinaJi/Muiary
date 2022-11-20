7b1115fad9fe645f8e60e7e8e0fca72e86f4dab9
MinaJi <minaaa0820@gmail.com>
2022-11-20 22:47:34 +0900

    1120

--- muiary-react-app/src/theme/GlobalStyle.js
+++ muiary-react-app/src/theme/GlobalStyle.js
@@ -7,10 +7,12 @@ body {
     height: 100vh;
     background-color: ${(props) => props.theme.bgColor};
     font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
     /* color: #232931; */
     color: ${(props) => props.theme.textColor}
 }
 button {
     font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
 }
 `;
