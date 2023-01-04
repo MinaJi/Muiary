09098f0750c2517591b966942288d54e74224642
MinaJi <minaaa0820@gmail.com>
2023-01-04 22:45:42 +0900

    editor

--- muiary-react-app/src/pages/CreateItem.jsx
+++ muiary-react-app/src/pages/CreateItem.jsx
@@ -1,13 +1,18 @@
-import { Grid } from "@mui/material";
+import { Divider, Grid } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
-import { RiAddCircleLine } from "react-icons/ri";
+import { IoIosAddCircleOutline } from "react-icons/io";
 import SearchModal from "../components/SearchModal";
 import { useState } from "react";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../firebase-config";
 import { UserAuth } from "../context/AuthContext";
 import MainHeader from "../components/MainHeader";
+import { DotIcon } from "../assets/svgs/index";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useNavigate } from "react-router-dom";
+import Editor from "../components/Editor";
 
 const StyledContainer = styled(Grid)`
   && {
@@ -20,33 +25,43 @@ const StyledContainer = styled(Grid)`
       border-radius: 10px;
     }
     .form-wrapper {
-      width: 700px;
+      width: 1000px;
       padding: 2rem;
-      border-radius: 20px;
-      border: 1px solid silver;
+      /* border-radius: 20px; */
+      /* border: 1px solid silver; */
+    }
+    .songdata-wrapper {
+      padding: 10px;
+    }
+    .artwork {
+      img {
+        width: 230px;
+        height: 230px;
+        border-radius: 10px;
+        box-shadow: rgba(97, 97, 97, 0.434) 0px 5px 15px;
+      }
     }
     .add {
-      width: 150px;
-      height: 150px;
-      border: 1px solid black;
+      width: 230px;
+      height: 230px;
+      border: 1px solid #eaeaea;
       border-radius: 10px;
+      box-shadow: rgba(97, 97, 97, 0.158) 0px 0px 20px;
     }
     .icon-btn {
       width: 50px;
       height: 50px;
-      background-color: inherit;
+      background-color: transparent;
       border: none;
       cursor: pointer;
       display: block;
       margin: 0 auto;
-      margin-top: 50px;
-      font-size: 30px;
+      padding-top: 100px;
+      font-size: 40px;
+      color: silver;
       :hover {
         color: #f73859;
       }
-    }
-    .songdata-wrapper {
-      background-color: lightblue;
     }
     .title {
       height: 40px;
@@ -69,7 +84,29 @@ const Btn = styled.button`
   }
 `;
 
+const SongDataGrid = styled(Grid)`
+  && {
+    padding: 10px 10px 10px 20px;
+    line-height: 32px;
+    font-size: 18px;
+    .trackname {
+      font-size: 32px;
+      font-weight: 600;
+    }
+    .artistname {
+      font-size: 25px;
+      color: #f73859;
+      font-weight: 500;
+    }
+    .collectionname {
+      font-size: 16px;
+      color: #a9a9a9;
+    }
+  }
+`;
+
 function CreateItem() {
+  const navi = useNavigate();
   const { user } = UserAuth();
   const [openModal, setOpenModal] = useState(false);
   const [songData, setSongData] = useState("");
@@ -78,43 +115,80 @@ function CreateItem() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await addDoc(collection(db, "boardItems"), {
+    await addDoc(collection(db, "users", `${user.uid}`, "boardItems"), {
       userId: user.uid,
       title: title,
       contents: contents,
       musicItem: songData,
+      like: false,
+      saved: false,
     });
     console.log("글작성완료~");
+    navi(-1);
   };
 
   return (
     <>
       <MainHeader />
-      {/* <img src={songData.artworkUrl100} alt="none" width="500px" /> */}
       <StyledContainer container direction="column" alignItems="center">
         <Grid item className="form-wrapper">
           <form onSubmit={handleSubmit}>
             <Grid container className="songdata-wrapper">
-              <Grid item xs={3}>
-                <div className="add">
-                  <button
-                    className="icon-btn"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      setOpenModal(true);
-                    }}
-                  >
-                    <RiAddCircleLine />
-                  </button>
-                </div>
-              </Grid>
-              <Grid item xs={9}>
+              <Grid item>
                 {songData ? (
-                  <div>
-                    <p>{songData.collectionName}</p>
+                  <div className="artwork">
+                    <img src={songData.artworkUrl100} alt="albumArtwork" />
                   </div>
                 ) : (
-                  <div>
+                  <div className="add">
+                    <button
+                      className="icon-btn"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setOpenModal(true);
+                      }}
+                    >
+                      <IoIosAddCircleOutline />
+                    </button>
+                  </div>
+                )}
+              </Grid>
+              <Grid item>
+                {songData ? (
+                  <SongDataGrid container direction="column">
+                    <Grid item className="trackname">
+                      {songData.trackName}
+                    </Grid>
+                    <Grid item className="artistname">
+                      {songData.artistName}
+                    </Grid>
+                    <Grid item className="collectionname">
+                      {songData.collectionName}
+                      <DotIcon />
+                      {songData.releaseDate}
+                    </Grid>
+                    <Grid item>
+                      <Divider />
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setOpenModal(true);
+                          setSongData("");
+                        }}
+                      >
+                        다시검색?
+                      </button>
+                    </Grid>
+                  </SongDataGrid>
+                ) : (
+                  <div
+                    style={{
+                      padding: "10px",
+                      paddingLeft: "20px",
+                      fontSize: "23px",
+                      color: "silver",
+                    }}
+                  >
                     <p>No songs selected...</p>
                   </div>
                 )}
@@ -129,10 +203,11 @@ function CreateItem() {
             </Grid>
             <Grid item className="contents-wrapper">
               <p>Contents</p>
-              <textarea
+              {/* <textarea
                 className="contents"
                 onChange={(e) => setContents(e.target.value)}
-              />
+              /> */}
+              <Editor />
             </Grid>
             <Grid item>
               <Btn type="submit">Submit</Btn>
